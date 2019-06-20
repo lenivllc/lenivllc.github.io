a1fb35a7c4010a40d0a56c5fa3eec7e3319e8687
lenivllc <lenivserver@gmail.com>
2019-06-20 14:51:26 +0300

    Updates

---
+++ precache-manifest.fa7ddb54080c8f6ab10da5d290765c89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa623e6fec0044a2b5893f3e3c388957",
+    "url": "/lenivllc/lenivllc.github.io/index.html"
+  },
+  {
+    "revision": "8aff499b10b55ed73663",
+    "url": "/lenivllc/lenivllc.github.io/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/lenivllc/lenivllc.github.io/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "8aff499b10b55ed73663",
+    "url": "/lenivllc/lenivllc.github.io/static/js/main.f90a46ee.chunk.js"
+  },
+  {
+    "revision": "6643b9a815dd061903f7",
+    "url": "/lenivllc/lenivllc.github.io/static/js/runtime~main.a6019ee2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lenivllc/lenivllc.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/lenivllc/lenivllc.github.io/precache-manifest.fa7ddb54080c8f6ab10da5d290765c89.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lenivllc/lenivllc.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
